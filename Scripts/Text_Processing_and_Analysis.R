318ac8fbd29d22e4723d6bcdb5944d3d8b3cc8a1
Matt Denny <matthewjdenny@gmail.com>
2016-05-26 09:04:10 -0400

    example reading in a corpus and looking at it

---
+++ Scripts/Text_Processing_and_Analysis.R
@@ -0,0 +1,58 @@
+# Text Processing and Analysis
+# Developed by: Matthew J. Denny
+# email: mdenny@psu.edu with questions or comments
+
+# clear out environment
+rm(list = ls())
+
+# preliminaries, make sure we have the right packages downloaded
+# install.packages("quanteda", dependencies = TRUE)
+# install.packages("stringr", dependencies = TRUE)
+# install.packages("devtools", dependencies = TRUE)
+# devtools::install_github("matthewjdenny/SpeedReader")
+
+# load packages
+require(stringr)
+require(quanteda)
+# require(SpeedReader)
+
+# set working directory (you will need to change this for your computer)
+setwd("~/Dropbox/RA_and_Consulting_Work/ISSR_Practical_Scraping_2016/Data/Bill_Text")
+
+# read in documents
+documents <- rep("", length = 100)
+# loop over documents
+for (i in 1:100) {
+    cat("currently working on bill:",i,"\n")
+    # set the current bill number
+    ind <- 97000 + i
+    # get the text of the bill
+    text <- readLines(paste("Bill_",ind,".txt", sep = ""))
+    # collapse it together into a string and store it in a vector
+    documents[i] <- paste0(text,collapse = " ")
+}
+
+# use quanteda to create a document term matrix
+doc_term_matrix <- quanteda::dfm(documents,
+                                 toLower = TRUE,
+                                 removeNumbers = TRUE,
+                                 removePunct = TRUE,
+                                 removeSeparators = TRUE,
+                                 removeTwitter = FALSE,
+                                 stem = FALSE,
+                                 language = "english")
+
+# look at some of the vocabulary
+head(doc_term_matrix@Dimnames$features)
+
+# get column sums
+word_counts <- colSums(doc_term_matrix)
+
+# order word counts
+word_counts <- word_counts[order(word_counts, decreasing = TRUE)]
+
+# top words
+head(word_counts,n = 20)
+
+# bottom words
+tail(word_counts,n = 20)
